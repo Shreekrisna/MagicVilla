4e7000ecae450c1fa35514fc6be9cc9f75debd7a
Shreekrisna <shree.kris810@gmail.com>
2023-12-10 22:12:26 +0545

    validation,datastore,produceresponsetype

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,19 +1,47 @@
-﻿using MagicVilla_VillaAPI.Models;
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
+    //[Route("api/controller")] 
     [Route("api/VillaAPI")] //This will be the route for this controller
     [ApiController]
     public class VillaAPIController:ControllerBase
     {
-        public IEnumerable<Villa> GetVillas()
+        [HttpGet]//here httpget is the endpoint and we have to define httpget in top i.e right here
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDto>> GetVillas()
         {
-            return new List<Villa>
+            return Ok(VillaStore.villalist);
+        }
+
+        //if we want to get the one villa based on the id
+        //[HttpGet("id:int")]
+        [HttpGet("id")]//if we dont define the http verb,it defaults to HTTPGET
+                       //To define multiple response that can be produced we will use producesresponsetype attribute
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        //[ProducesResponseType(200)]
+        //[ProducesResponseType(404)]
+        //[ProducesResponseType(400)]
+
+        public ActionResult<VillaDto> GetVilla(int id)//return type will be actionresult of type villaDto
+        {
+            //Validation
+            if(id==0)
             {
-                new Villa{Id=1,Name="Pool View"},
-                new Villa{Id=2,Name="Beach View"}
-            };
+                return BadRequest();
+            }
+            var villa = VillaStore.villalist.FirstOrDefault(u => u.Id == id);
+            if(villa==null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
         }
 
     }
